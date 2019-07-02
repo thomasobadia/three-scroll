c77fd34d56329d6e34c434b1f8f49ed7218d7722
Thomas Obadia <thomas.obadia@hetic.net>
2019-07-02 10:10:58 +0200

    scroll inertia fix

--- scripts/main.js
+++ scripts/main.js
@@ -787,11 +787,11 @@ const init = () => {
                 TweenMax.to(progressContainer,0.25,{opacity:0, onComplete:()=> {
                     progressContainer.style.display = 'none'
                 }})
-                let move = scale(offset.y, -300, 300, -20, 20)
+                let move = scale(offset.y, -300, 300, -50, 50)
 
                 if(camera.position.z - move <= 4.2){
                     // camera.position.z -= move;
-                    TweenMax.to(camera.position,2, { ease: Power0.easeInOut, z: "-=" + move, overwrite : "none"});
+                    TweenMax.to(camera.position,1.5, { ease: Power0.easeInOut, z: "-=" + move, overwrite : "none"});
                 } 
 
                 if(camera.position.z - move >= 4){
