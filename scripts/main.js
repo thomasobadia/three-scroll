594884652f378663698b866b13bb88b3f9a5aed4
Thomas Obadia <thomas.obadia@hetic.net>
2019-07-04 10:19:43 +0200

    fr

--- scripts/main.js
+++ scripts/main.js
@@ -819,7 +819,7 @@ const init = () => {
         overlayYearMobile.textContent = obj.name
         overlayContentMobile.innerHTML = obj.content
         overlayImageMobile.srcset = obj.material.map.image.currentSrc
-        document.querySelector('navbar-content').style.display = 'none'
+        document.querySelector('.navbar-content').style.display = 'none'
         overlayContainerMobile.style.display = 'flex'
         overlayCloseMobile.style.display = 'block'
         TweenMax.to(overlayContainerMobile,0.5, {ease: Power2.easeInOut, opacity:1, scale: 1});
@@ -828,7 +828,7 @@ const init = () => {
     }
 
     const closeImageMobile = () => {
-        document.querySelector('navbar-content').style.display = 'flex'
+        document.querySelector('.navbar-content').style.display = 'flex'
 
         TweenMax.to(overlayCloseMobile,0.5, {ease: Power2.easeInOut,  opacity:0, scale : 0.9, onComplete: () => overlayContainer.style.display = 'none'});
         TweenMax.to(overlayContainerMobile,0.5, {ease: Power2.easeInOut,  opacity:0, scale : 0.9, onComplete: () => overlayContainerMobile.style.display = 'none'});
